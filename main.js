399e0fd1aefca256497ea7ccf6ffe2c6ec6475ef
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-06-27 22:04:46 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function age() {
+    var d1 = document.getElementById('date').value;
+    var m1 = document.getElementById('month').value;
+    var y1 = document.getElementById('year').value;
+  
+}
