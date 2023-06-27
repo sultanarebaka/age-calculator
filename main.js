704ea2dd755ad9905d04f8cc1aebb28db067d14d
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-06-27 23:04:44 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ function age() {
     var d1 = document.getElementById('date').value;
     var m1 = document.getElementById('month').value;
     var y1 = document.getElementById('year').value;
-  
 }
+var date = new Date();
+var d2 = date.getDate();
+var m2 = 1 + date.getMonth();
